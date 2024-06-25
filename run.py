4232f476083e36e2ad64ce7c53f061db8c250ec5
Farha Islam <farha.islam@students.codeinstitute.net>
2024-06-25 19:10:24 +0000

    Refactor initialization function

--- run.py
+++ run.py
@@ -30,75 +30,51 @@ serviceDetails = SHEET.worksheet('ServiceDetail')
 data = serviceDetails.get_all_values()
 
 print(data)
+       
 
-class Service:
+
+def main_menu():
     """
-    This class is responsible for controlling the data. It shows the welcome page and takes user input and
-    follow the actions. 
+    Displays title, the main menu, handles user input for various
+    customer-related actions, and directs the user to the
+    appropriate method based on their choice.
     """
-    def __init__(self):
-        """
-        Initializes the ConsoleCRM instance and prints a welcome message.
-        """
+    console.clear()
+    print(pyfiglet.figlet_format(
+    "Service Detail", justify="center", width=80))
 
-    def run(self):
-        """
-        Starts the Service application by displaying the main menu.
-        """
-        self.main_menu()
+    while True:  # Loop until a valid choice is made
+        print("List of actions")
+        table = Table(title="Menu", width=console.width)
 
-    def main_menu(self):
-        """
-        Displays the main menu, handles user input for various
-        customer-related actions, and directs the user to the
-        appropriate method based on their choice.
-        """
-        console.clear()
-        print(pyfiglet.figlet_format(
-            "Service Detail", justify="center", width=80))
+        table.add_column("Option", justify="center", style="blue")
+        table.add_column("Description", justify="center", style="blue")
 
+        table.add_row("1", "Show all services")
+        table.add_row("2", "Search service")
+        table.add_row("3", "Edit service")
+        table.add_row("4", "Add new service")
+        table.add_row("5", "Delete service")
+        table.add_row("6", "Exit")
 
-        while True:  # Loop until a valid choice is made
-            print("List of actions")
-            table = Table(title="Menu", width=console.width)
+        print(table)
+        choice = input("> ").strip()
 
-            table.add_column("Option", justify="center", style="blue")
-            table.add_column("Description", justify="center", style="blue")
-
-            table.add_row("1", "Show all services")
-            table.add_row("2", "Search service")
-            table.add_row("3", "Edit customer")
-            table.add_row("4", "Add new service")
-            table.add_row("5", "Delete service")
-            table.add_row("6", "Exit")
-
-            print(table)
-            choice = input("> ").strip()
-
-            if choice == '1':
-                self.show_all_services()
-            elif choice == '2':
-                self.search_service()
-            elif choice == '3':
-                self.edit_service()
-            elif choice == '4':
-                self.add_new_service()
-            elif choice == '5':
-                self.delete_service()
-            elif choice == '6':
-                console.print("Program Exists!")
-                return
-            else:
-                console.print("Invalid choice, please select a valid option.")
-
-    def introduction(self):
-        """
-        Display introduction message various action menues. It also 
-        directs the user to the appropriate method based on their choice.
-        """
-
-
-
+        if choice == '1':
+            show_all_services()
+        elif choice == '2':
+            search_service()
+        elif choice == '3':
+            edit_service()
+        elif choice == '4':
+            add_new_service()
+        elif choice == '5':
+            delete_service()
+        elif choice == '6':
+            print("Program Exists!")
+            return
+        else:
+            console.print("Invalid choice, please select a valid option.")
 
 
 def main():
@@ -106,7 +82,7 @@ def main():
     Creates an instance of the Service Detail application and starts it by calling
     the 'run' method.
     """
-    app = Service()
-    app.run()
+    main_menu()
+
 
 main()
